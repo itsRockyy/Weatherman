a343c93c9cd551b398cd036f25adac274ec15025
Abhijeet Saxena <saxenaabhijeet@outlook.com>
2018-04-29 00:15:07 +0530

    Adding responsiveness

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,7 @@
 */
 document.getElementById("locate").addEventListener("click", getLocation);
 document.getElementById("search").addEventListener("click", searchWetaher);
+// document.getElementById("changeTemp").addEventListener("click", changeTemp);
 
 function getLocation() {
   console.log("Getting Location");
@@ -56,14 +57,26 @@ function searchWetaher(e) {
     .then(response => response.json())
     .then(json => {
       console.log(JSON.stringify(json));
-      var temp = Math.round(json.main.temp - 273.15);
+      let temp = Math.round(json.main.temp - 273.15);
+      let tempinF = Math.round(json.main.temp * 1.8 - 459.67);
+      let windspeed = Math.round(json.wind.speed);
+      let direction = toTextualDescription(json.wind.deg);
+      var sunrise = new Date(json.sys.sunrise * 1000);
+      var sunset = new Date(json.sys.sunset * 1000);
 
       document.getElementById("card1").innerHTML = `
       <ul>
-      <li>${json.name}</li>
+      <li style="margin-top: 1.5rem"><span class="badge badge-pill badge-dark" style="font-size: 2rem">${
+        json.name
+      }</span></li>
       <li>Latitude: ${json.coord.lat}</li>
       <li>Longitude: ${json.coord.lon}</li>
-    <li><img src="assets/wind.png" alt="wind"></li>
+    <li>
+    <li>
+    <img src="assets/windmill.png" alt="wind" style="margin:0">
+    <i class="wi wi-wind ${direction}" style="margin-left:-1rem; margin-right:0;font-size: 1.8rem"></i>${3.6 *
+        windspeed} kmph
+    </li>
     </ul>
       `;
 
@@ -71,7 +84,8 @@ function searchWetaher(e) {
       <ul>
       <li>${json.weather[0].main}</li>
       <li style="text-transform: capitalize">${json.weather[0].description}</li>
-      <li><i class="wi wi-thermometer"></i>${temp}</li>
+      <li style="font-size: 5rem"><i class="wi wi-thermometer"></i>${temp}<i class="wi wi-celsius"></i></li>
+      <button id="changeTemp" class="btn btn-outline-secondary" onclick="changeTemp(this)">Don't understand Celsius !</button>
       </ul>
       `;
 
@@ -80,41 +94,32 @@ function searchWetaher(e) {
       <li> <i class="wi wi-humidity"></i> ${json.main.humidity} %</li>
       <li> <i class="wi wi-barometer"></i> ${json.main.pressure} millibars </li>
       <li> <i class="fas fa-eye"></i>  ${json.visibility} m</li>
+      <li> <i class="wi wi-sunrise"></i>  ${sunrise.toLocaleTimeString()}</li>
+      <li> <i class="wi wi-sunset"></i>  ${sunset.toLocaleTimeString()}</li>
       </ul>
       `;
     })
     .catch(error => console.log(error));
 }
 
+function changeTemp(e) {
+  console.log(e.innerHTML);
+}
+
+// Wind Direction
+function toTextualDescription(degree) {
+  if (degree > 337.5) return "wi-towards-n";
+  if (degree > 292.5) return "wi-towards-nw";
+  if (degree > 247.5) return "wi-towards-w";
+  if (degree > 202.5) return "wi-towards-sw";
+  if (degree > 157.5) return "wi-towards-s";
+  if (degree > 122.5) return "wi-towards-se";
+  if (degree > 67.5) return "wi-towards-e";
+  if (degree > 22.5) return "wi-towards-ne";
+  return "wi-towards-n";
+}
+
 // JQuery Initializers
 $(function() {
   $('[data-toggle="tooltip"]').tooltip();
 });
-
-/*
-JSON{
-   "name":"Mathura",
-   "sys":{"country":"IN"},
-    "coord":{  
-      "lon":77.69,
-      "lat":27.5
-   },
-   "weather":[  
-      {  
-         "main":"Clear",
-         "description":"clear sky",
-         "icon":"01d"
-      }
-   ],
-   "main":{  
-      "temp":307.543,
-      "humidity":13,
-      "temp_min":307.543,
-      "temp_max":307.543,
-     },
-   "wind":{  
-      "speed":4.76,
-      "deg":315.502
-   },
-}
-*/
