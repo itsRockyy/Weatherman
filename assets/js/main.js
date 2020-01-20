fb830d5ba428a30eb9c80feb230e292b5a02b83c
Abhijeet Saxena <saxenaabhijeet@outlook.com>
2020-01-21 00:00:00 +0530

    Update API calls

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,78 +1,55 @@
-// All event Listeners
-document.getElementById("locate").addEventListener("click", getLocation);
-document
-  .getElementById("search")
-  .addEventListener("click", searchWeatherByCity);
-
 // Global variable for user temperature preference
-var tempVar = "Celsius";
-var latitude = localStorage.getItem("WeatherManLat");
-var longitude = localStorage.getItem("WeatherManLong");
+let tempVar = "Celsius";
+let latitude = localStorage.getItem("WeatherManLat");
+let longitude = localStorage.getItem("WeatherManLong");
 
-function getLocation() {
+const getLocation = () =>
   navigator.geolocation.getCurrentPosition(geo_success, geo_error, geo_options);
-}
 
 // This is executed after succesfully locating clients location
 // Also sets the coordinates in local storage to prevent overhead time in future
-function geo_success(position) {
+const geo_success = (position) => {
   latitude = position.coords.latitude;
   longitude = position.coords.longitude;
   localStorage.setItem("WeatherManLong", longitude);
   localStorage.setItem("WeatherManLat", latitude);
   searchWeatherByCoordinates(latitude, longitude);
-}
+};
 
 // Error while geolocating, alert user to choose other option
-function geo_error(e) {
+const geo_error = (e) => {
   searchWeatherByCoordinates(latitude, longitude);
-}
+};
 
 // This sets the options parameter while geolocating client
-var geo_options = {
+const geo_options = {
   enableHighAccuracy: false,
   maximumAge: Infinity,
-  timeout: 27000
+  timeout: 27000,
 };
 
 //  OpenweatherMap API call by city
-function searchWeatherByCity() {
+const searchWeatherByCity = () => {
   let searchParam = document.getElementById("searchParam").value;
-  fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-      searchParam +
-      "&appid=175bce777dda85d8fbff08fad9286267"
-  )
-    .then(response => response.json())
-    .then(json => {
-      console.log(JSON.stringify(json));
-      mapResponseToUI(json);
-    })
-    .catch(error => $("#errorModal").modal("show"));
-}
+  fetch(`https://itsrockyy.herokuapp.com/api/weatherman?q=${searchParam}`)
+    .then((response) => response.json())
+    .then((json) => mapResponseToUI(json))
+    .catch(() => $("#errorModal").modal("show"));
+};
 
 //  OpenweatherMap API call by coordinates
-function searchWeatherByCoordinates(latitude, longitude) {
-  let searchParam = document.getElementById("searchParam").value;
+const searchWeatherByCoordinates = (latitude, longitude) => {
   fetch(
-    "http://api.openweathermap.org/data/2.5/weather?lat=" +
-      latitude +
-      "&lon=" +
-      longitude +
-      "&appid=175bce777dda85d8fbff08fad9286267"
+    `https://itsrockyy.herokuapp.com/api/weatherman?lat=${latitude}&lon=${longitude}`
   )
-    .then(response => response.json())
-    .then(json => {
-      console.log(JSON.stringify(json));
-      mapResponseToUI(json);
-    })
-    .catch(error => $("#errorModal").modal("show"));
-}
+    .then((response) => response.json())
+    .then((json) => mapResponseToUI(json))
+    .catch(() => $("#errorModal").modal("show"));
+};
 
 // map json response to UI
-function mapResponseToUI(json) {
+const mapResponseToUI = (json) => {
   let temp = Math.round(json.main.temp - 273.15);
-  let tempinF = Math.round(json.main.temp * 1.8 - 459.67);
   let windspeed = Math.round(json.wind.speed);
   let direction = getWindIcon(json.wind.deg);
   var sunrise = new Date(json.sys.sunrise * 1000);
@@ -113,29 +90,27 @@ function mapResponseToUI(json) {
       `;
 
   document.getElementById("cardbox").style.visibility = "visible";
-}
+};
 
 // convert Celsius to Fahrenheit & vice-versa
-function changeTemp(e) {
-  let temp = document.getElementById("temp").innerText;
-  if (tempVar === "Celsius") {
-    tempVar = "Fahrenheit";
-    var tempConverted = Math.round(1.8 * temp + 32);
-    var tempIcon = "wi-fahrenheit";
-  } else {
-    tempVar = "Celsius";
-    var tempIcon = "wi-celsius";
-    var tempConverted = Math.round((temp - 32) / 1.8);
-  }
+const changeTemp = (e) => {
+  const temp = document.getElementById("temp").innerText;
+
+  tempVar = tempVar === "Celsius" ? "Fahrenheit" : "Celsius";
+  const tempConverted =
+    tempVar !== "Celsius"
+      ? Math.round(1.8 * temp + 32)
+      : Math.round((temp - 32) / 1.8);
+  const tempIcon = tempVar !== "Celsius" ? "wi-fahrenheit" : "wi-celsius";
 
   document.getElementById(
     "temp"
   ).innerHTML = `<i class="wi wi-thermometer"></i>${tempConverted}<i class="wi ${tempIcon}"></i>`;
   e.innerHTML = `Don't understand ${tempVar} !`;
-}
+};
 
 // set Wind Direction Icons
-function getWindIcon(degree) {
+const getWindIcon = (degree) => {
   if (degree > 337.5) return "wi-towards-n";
   if (degree > 292.5) return "wi-towards-nw";
   if (degree > 247.5) return "wi-towards-w";
@@ -145,10 +120,16 @@ function getWindIcon(degree) {
   if (degree > 67.5) return "wi-towards-e";
   if (degree > 22.5) return "wi-towards-ne";
   return "wi-towards-n";
-}
+};
 
 // JQuery Initializers
-$(function() {
+$(function () {
   $('[data-toggle="tooltip"]').tooltip();
   $("#errroModal").modal({ show: false });
 });
+
+// All event Listeners
+document.getElementById("locate").addEventListener("click", getLocation);
+document
+  .getElementById("search")
+  .addEventListener("click", searchWeatherByCity);
